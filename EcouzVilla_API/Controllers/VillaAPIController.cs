cfcf205c469af16ffb04fc8ef7a953664e90e14b
iman <dev.imansabet@gmail.com>
2024-07-28 15:40:09 +0330

    Add Villa to Project (Controller - model  - dto)

---
+++ EcouzVilla_API/Controllers/VillaAPIController.cs
@@ -0,0 +1,22 @@
+﻿using EcouzVilla_API.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcouzVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerator<VillaDTO> GetVillas()
+        {
+
+        }
+        [HttpGet]
+        public VillaDTO GetVilla(int id)
+        {
+
+        }
+    }
+}
