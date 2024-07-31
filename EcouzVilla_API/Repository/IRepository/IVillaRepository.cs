8c9179b24ca3dd670f6e9bf5215cdea1b74593d6
iman <dev.imansabet@gmail.com>
2024-07-31 20:32:23 +0330

    Add Repository Interface

---
+++ EcouzVilla_API/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,14 @@
+﻿using EcouzVilla_API.Models;
+using System.Linq.Expressions;
+
+namespace EcouzVilla_API.Repository.IRepository
+{
+    public interface IVillaRepository
+    {
+        Task<List<Villa>> GetAll(Expression<Func<Villa>> filter = null);
+        Task<Villa> Get(Expression<Func<Villa>> filter = null,bool tracked = true);
+        Task Create(Villa entity);
+        Task Remove(Villa entity);
+        Task Save();
+    }
+}
