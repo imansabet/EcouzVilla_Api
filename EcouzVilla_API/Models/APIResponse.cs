e8b143a80505743cbb41b5d04f332d791dde7134
iman <dev.imansabet@gmail.com>
2024-08-01 15:10:41 +0330

    Add Villa Number Dto and Details

--- EcouzVilla_API/Models/APIResponse.cs
+++ EcouzVilla_API/Models/APIResponse.cs
@@ -5,7 +5,7 @@ namespace EcouzVilla_API.Models
     public class APIResponse
     {
         public HttpStatusCode StatusCode { get; set; }
-        public bool IsSuccess { get; set; }
+        public bool IsSuccess { get; set; } = true;
         public List<string> ErrorMessages { get; set; }
         public object Result { get; set; }
     }
