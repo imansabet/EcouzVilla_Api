105af900fa2eb04cecff3d13303b5257921e9e54
iman <dev.imansabet@gmail.com>
2024-08-01 13:57:21 +0330

    Generic Repository

---
+++ EcouzVilla_API/Repository/IRepository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using EcouzVilla_API.Models;
+using System.Linq.Expressions;
+
+namespace EcouzVilla_API.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> ?filter = null);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true);
+        Task CreateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task UpdateAsync(T entity);
+
+        Task SaveAsync();
+    }
+}
