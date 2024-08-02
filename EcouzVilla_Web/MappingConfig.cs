12979cea19a578ced8e5bdd2a1bd539d4c0e781f
iman <dev.imansabet@gmail.com>
2024-08-02 09:09:52 +0330

    Config AutoMapper in Web Project

---
+++ EcouzVilla_Web/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using EcouzVilla_Web.Models.Dto;
+
+namespace EcouzVilla_Web
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+            CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+
+            CreateMap<VillaNumberDTO, VillaNumberCreateDTO>().ReverseMap();
+            CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+
+            
+        }
+    }
+}
