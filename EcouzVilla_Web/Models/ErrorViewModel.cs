59e2c541e6652ff21d2ba3b709cc9fd09911baa9
iman <dev.imansabet@gmail.com>
2024-08-02 08:32:50 +0330

    Setup mvc project

---
+++ EcouzVilla_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcouzVilla_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
