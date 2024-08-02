afa556f067ce5e80ef853dd548e76a0073c08535
iman <dev.imansabet@gmail.com>
2024-08-02 08:45:32 +0330

    Add Web Project Dto and Api Models  (ApiRequest)

---
+++ EcouzVilla_Web/Models/APIRequest.cs
@@ -0,0 +1,11 @@
+﻿using static EcouzVilla_Utility.SD;
+
+namespace EcouzVilla_Web.Models
+{
+    public class APIRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+    }
+}
