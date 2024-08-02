66818e0d020e7818b28bf6be630c60c24e9ca670
iman <dev.imansabet@gmail.com>
2024-08-02 10:28:34 +0330

    Calling Villa Api

---
+++ EcouzVilla_Web/Controllers/VillaController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using EcouzVilla_Web.Models;
+using EcouzVilla_Web.Models.Dto;
+using EcouzVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace EcouzVilla_Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly IVillaService _villaService;
+        private readonly IMapper _mapper;
+        public VillaController(IVillaService villaService, IMapper mapper)
+        {
+            _villaService = villaService;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> IndexVilla()
+        {
+            List<VillaDTO> list = new();
+
+            var response = await _villaService.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
