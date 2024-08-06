cd5f4ea8ee430d4ab8401acc238239fa46313a2f
iman <dev.imansabet@gmail.com>
2024-08-06 22:28:10 +0330

    Add & Register Auth Service

---
+++ EcouzVilla_Web/Services/AuthService.cs
@@ -0,0 +1,40 @@
+﻿using EcouzVilla_Utility;
+using EcouzVilla_Web.Models.Dto;
+using EcouzVilla_Web.Models;
+using EcouzVilla_Web.Services.IServices;
+
+namespace EcouzVilla_Web.Services
+{
+    public class AuthService : BaseService, IAuthService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private string villaUrl;
+
+        public AuthService(IHttpClientFactory clientFactory, IConfiguration configuration) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+            villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+
+        }
+
+        public Task<T> LoginAsync<T>(LoginRequestDTO obj)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = obj,
+                Url = villaUrl + "/api/UsersAuth/login"
+            });
+        }
+
+        public Task<T> RegisterAsync<T>(RegisterationRequestDTO obj)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = obj,
+                Url = villaUrl + "/api/UsersAuth/register"
+            });
+        }
+    }
+}
---
+++ EcouzVilla_Web/Services/IServices/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using EcouzVilla_Web.Models.Dto;
+
+namespace EcouzVilla_Web.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<T> LoginAsync<T>(LoginRequestDTO objToCreate);
+        Task<T> RegisterAsync<T>(RegisterationRequestDTO objToCreate);
+    }
+}
