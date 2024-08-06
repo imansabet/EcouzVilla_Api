dbed6c74c8194b6e575c408f7f4257e8c2a32410
iman <dev.imansabet@gmail.com>
2024-08-06 21:04:14 +0330

    implement user repository - Generate token on successful loign

--- EcouzVilla_API/Repository/UserRepository.cs
+++ EcouzVilla_API/Repository/UserRepository.cs
@@ -2,15 +2,23 @@
 using EcouzVilla_API.Models;
 using EcouzVilla_API.Models.Dto;
 using EcouzVilla_API.Repository.IRepository;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace EcouzVilla_API.Repository
 {
     public class UserRepository : IUserRepository
     {
         private readonly ApplicationDbContext _db;
-        public UserRepository(ApplicationDbContext db)
+        private string secretKey;
+
+        public UserRepository(ApplicationDbContext db, IConfiguration configuration)
         {
             _db = db;
+            secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+
         }
         public bool IsUniqueUser(string username)
         {
@@ -24,6 +32,37 @@ namespace EcouzVilla_API.Repository
 
         public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
         {
+            var user = _db.LocalUsers
+                    .FirstOrDefault(
+                    u => u.UserName.ToLower() == loginRequestDTO.UserName.ToLower() &&
+                    u.Password == loginRequestDTO.Password);
+            if (user == null)
+            {
+                return null;
+            }
+            // user was found ?  generate Token
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(secretKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                           {
+                                new Claim(ClaimTypes.Name, user.Id.ToString()),
+                                new Claim(ClaimTypes.Role, user.Role)
+                           }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            LoginResponseDTO loginResponseDTO = new LoginResponseDTO()
+            {
+                Token = tokenHandler.WriteToken(token),
+                User = user
+            };
+            return loginResponseDTO;
+
 
         }
 
