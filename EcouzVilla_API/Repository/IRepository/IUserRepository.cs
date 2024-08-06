fbf900f73671b1d44a5ca1586c029233083ccd73
iman <dev.imansabet@gmail.com>
2024-08-06 19:45:18 +0330

    Add User Repository

---
+++ EcouzVilla_API/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using EcouzVilla_API.Models;
+using EcouzVilla_API.Models.Dto;
+
+namespace EcouzVilla_API.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<LocalUser> Register(RegisterationRequestDTO  registerationRequestDTO);
+    }
+}
---
+++ EcouzVilla_API/Repository/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using EcouzVilla_API.Data;
+using EcouzVilla_API.Models;
+using EcouzVilla_API.Models.Dto;
+using EcouzVilla_API.Repository.IRepository;
+
+namespace EcouzVilla_API.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool IsUniqueUser(string username)
+        {
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+        }
+
+        public Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+        }
+    }
+}
