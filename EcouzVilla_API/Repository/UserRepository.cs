3152a0fa39c7f2f2826ac483e7396fd2e5540482
iman <dev.imansabet@gmail.com>
2024-08-06 19:55:25 +0330

    Implement User repository -> Register

--- EcouzVilla_API/Repository/UserRepository.cs
+++ EcouzVilla_API/Repository/UserRepository.cs
@@ -14,14 +14,32 @@ namespace EcouzVilla_API.Repository
         }
         public bool IsUniqueUser(string username)
         {
+            var user = _db.LocalUsers.FirstOrDefault(x => x.UserName == username);
+            if(user == null)
+            {
+                return true;
+            }
+            return false;
         }
 
         public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
         {
+
         }
 
-        public Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+        public async Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
         {
+            LocalUser user = new LocalUser()
+            {
+                UserName = registerationRequestDTO.UserName,
+                Password = registerationRequestDTO.Password,
+                Name = registerationRequestDTO.Name,
+                Role = registerationRequestDTO.Role
+            };
+            _db.LocalUsers.Add(user);
+            await _db.SaveChangesAsync();
+            user.Password = "";
+            return user;
         }
     }
 }
