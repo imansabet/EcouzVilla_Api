c83536ea83a39f008a8f0eddea900da1ed9ef644
iman <dev.imansabet@gmail.com>
2024-08-07 20:26:46 +0330

    Fix a wrong url due the home view problem

--- EcouzVilla_Web/Controllers/HomeController.cs
+++ EcouzVilla_Web/Controllers/HomeController.cs
@@ -11,13 +11,14 @@ namespace EcouzVilla_Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IVillaService _villaService;
+          private readonly IVillaService _villaService;
         private readonly IMapper _mapper;
         public HomeController(IVillaService villaService, IMapper mapper)
         {
             _villaService = villaService;
             _mapper = mapper;
         }
+
         public async Task<IActionResult> Index()
         {
             List<VillaDTO> list = new();
